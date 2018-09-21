05ceaae922d272cd52e4d26b2e4a201e632b1f95
Navin Ghimire <ghimire.navin@gmail.com>
2018-09-21 13:07:25 -0500

    Working on multiplication

--- infinitearithmetic.py
+++ infinitearithmetic.py
@@ -3,6 +3,7 @@ __email__ = "nghimire@uh.edu"
 __version__ = "1.0.0"
 from enum import Enum
 import re
+import sys
 class Node:
     carry = 0
     value = []
@@ -20,18 +21,13 @@ class OperatorType(Enum):
     MULTIPLY = "*"
 class Expression:
     operator = OperatorType
-    operand1 = ''
-    operand2 = ''
-    result =''
-    def __init__(self,operand1,operator,operand2):
+    operand1 = 0
+    operand2 = 0
+    def __init__(self,operand1,operand2,operator):
         self.operand1 = operand1
         self.operator = operator
         self.operand2 = operand2
 
-def add(operand1,operand2):
-    result = []
-    addListRecursively(operand1, operand2, result, len(operand2))
-    return result
 
 def addNodesRecursively(node1, node2, result, counter, carryToAdd):
     if counter == 0:
@@ -59,45 +55,60 @@ def processFile(file,dpn):
     if line.find("*") != -1:
         line = line.rstrip().split("*")
         Expression(line[0],line[1],OperatorType.MULTIPLY)
-
-
     elif line.find("+") != -1:
         line = line.rstrip().split("+")
-        Expression(line[0], line[1], OperatorType.ADD)
-        alignedOperands = alignOperands([line[0],line[1]],dpn)
-        result = add(alignedOperands[0],alignedOperands[1])
-        k = str(result[0].carry)
-        for node in result:
-            for l in node.value:
-                k = k+str(l)
-        print(k.lstrip('0'))
+        line = Expression(line[0], line[1], OperatorType.ADD)
+        r = []
+        pv = alignOperands([line.operand1,line.operand2],dpn)
+        addListRecursively(pv[0],pv[1],r,len(pv[0]))
+
+        print("%s+%s=%s" % (line.operand1,line.operand2,extractResult(r)))
+
     processFile(file,dpn)
 
+def extractResult(result):
+    a = str(result[0].carry)
+    for node in result:
+        for l in node.value:
+            a = a + str(l)
+    return(a.lstrip('0'))
 
 def alignOperands(li,dpn):
     a = li[0]
     b = li[1]
     if len(a) > len(b):
-        c = len(a)-len(b)
-        b = c*'0'+b
+        c = len(a) - len(b)
+        b = c * '0' + b
     elif len(a) < len(b):
-        c = len(b)-len(a)
-        a = c*'0'+a
+        c = len(b) - len(a)
+        a = c * '0' + a
     if len(a) % dpn != 0:
-        c = dpn - len(a)%dpn
-        a = c*'0'+a
-        b = c*'0'+b
-    cc = len(a)%dpn
-    operand1List = re.findall(dpn*'.',a[cc:])
-    operand2List = re.findall(dpn*'.',b[cc:])
-    operand1List = [Node(list(map(int,l))) for l in operand1List]
+        c = dpn - len(a) % dpn
+        a = c * '0' + a
+        b = c * '0' + b
+        #print(c)
+    #print(b)
+    cc = len(a) % dpn
+    operand1List = re.findall(dpn * '.', a[cc:])
+    operand2List = re.findall(dpn * '.', b[cc:])
+    operand1List = [Node(list(map(int, l))) for l in operand1List]
     operand2List = [Node(list(map(int, l))) for l in operand2List]
+
     return [operand1List,operand2List]
 
 def main():
-    dpn = 2
-    f = open('m', "r")
+    # extracting command line argument- filename and digits per node (dpn)
+    arguments = sys.argv[1].split(";")
+    filename = arguments[0].split("=")[1]
+    dpn = int(arguments[1].split("=")[1])
+
+    #opening file for processing
+    f = open(filename, "r")
+
+    #recursive fuction to process each lines
     processFile(f,dpn)
+
     f.close()
+
 if __name__ == "__main__":
     main()
